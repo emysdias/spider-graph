100e1553d5d0d4799d6a3cbaff9a9b248a934383
emysdias <diassousa@yahoo.com.br>
2023-05-06 19:26:53 -0300

    Testando rota do netlify

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,20 @@
 import "./style.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const redirectToResult = () => {
+    navigate("/resultado");
+  };
   return (
     <div className="container-home">
+      <button
+        className="container-home-button"
+        onClick={redirectToResult}
+        width={"1000px"}
+      >
+        Testando rotas do deploy
+      </button>
     </div>
   );
 };
